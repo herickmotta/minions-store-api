33ea62f0f27b7e14c25ea810adb32edada5adc29
herickmotta <herickrossato@gmail.com>
2021-03-21 23:04:18 -0300

    fix: delete ses

--- src/utils/ses.js
+++
@@ -1,9 +0,0 @@
-import { SES } from 'aws-sdk';
-
-const ses = new SES({
-  region: 'us-east-1',
-  accessKeyId: 'AKIAQV2YIDSPBK642MQX',
-  secretAccessKey: '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',
-});
-
-export default ses;
